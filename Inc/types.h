4dafc36f54206a6886d828c6840032d8a2248a7c
milanbijeljac <mbijeljac1995@gmail.com>
2026-08-15 15:56:09 +0200

    constexpr instead of #define

--- Inc/types.h
+++ Inc/types.h
@@ -3,21 +3,20 @@
 /* **************************************************
  *			    	INCLUDES					    *
  *************************************************  */
-#include <stdint.h>
-
+#include <cstdint>
 /* **************************************************
  *					DEFINES 					    *
  *************************************************  */
-
-#define OK 			0u
-#define NOK 		1u
-
-#define STD_OFF		0u
-#define STD_ON		1u
 
 typedef uint8_t  uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 typedef int8_t   sint8;
+
+constexpr uint8 OK = 0u;
+constexpr uint8 NOK = 1u;
+
+constexpr uint8 STD_OFF	= 0u;
+constexpr uint8 STD_ON = 1u;
 
