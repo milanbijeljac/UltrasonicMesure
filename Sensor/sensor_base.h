a072f0a6acd3131b3bee7ff8acb92a56805dd2a3
milanbijeljac <mbijeljac1995@gmail.com>
2026-08-15 15:36:15 +0200

    Adding dtor in protected

--- Sensor/sensor_base.h
+++ Sensor/sensor_base.h
@@ -15,6 +15,12 @@ namespace Sensor
 
 		std::string_view getName() const noexcept { return m_name; };
 
+	protected:
+		/* Protected + non-virtual: derived classes can destroy the base
+		 * subobject, but a SensorBase* cannot be deleted (which would be UB
+		 * with a non-virtual base destructor). No vtable cost. */
+		~SensorBase() = default;
+
 	private:
 		const std::string_view m_name;
 
